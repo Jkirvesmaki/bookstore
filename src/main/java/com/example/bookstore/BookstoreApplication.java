71e4d1de9b7e5777774731d57f365d7c5a2398a5
Jkirvesmaki <joonas.kirvesmaki@gmail.com>
2020-11-03 14:10:59 +0200

    viimesin

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -4,6 +4,7 @@ package com.example.bookstore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,8 @@ import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 import com.example.bookstore.model.Category;
 import com.example.bookstore.model.CategoryRepository;
+import com.example.bookstore.model.UserRepository;
+import com.example.bookstore.model.User;
 
 
 	@SpringBootApplication
@@ -24,7 +27,7 @@ import com.example.bookstore.model.CategoryRepository;
 			SpringApplication.run(BookstoreApplication.class, args);
 		}
 		@Bean
-		public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository) {
+		public CommandLineRunner bookDemo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 			return (args) -> {
 				
 	
@@ -32,9 +35,8 @@ import com.example.bookstore.model.CategoryRepository;
 				crepository.save(new Category("asd"));
 				crepository.save(new Category("asdaddasdafaf"));
 				brepository.save(new Book("The Capital", "Marx", 1890, "123-234", 99.90,crepository.findByName("asd").get(0)));
-	
-	
-				
+				urepository.save(new User("1", "joonas2", "$2y$12$.yqQrFCmU53msFJLZrSvF.CpbrsUr612Ac3vFblyw.tMW.VwrXsKS", "joonas.kirvesmaki@gmail.com", "ADMIN"));
+				urepository.save(new User("2", "joonas1", "$2y$12$.yqQrFCmU53msFJLZrSvF.CpbrsUr612Ac3vFblyw.tMW.VwrXsKS", "joonas.kirvesmaki@gmail.com", "user"));
 				log.info("fetch all books");
 				for (Book book : brepository.findAll()) {
 					log.info(book.toString());
--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,11 @@ import com.example.bookstore.model.CategoryRepository;
 @Controller
 public class BookController {
     
+    @RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }	
+	
     @Autowired
     private BookRepository repository;
 
@@ -29,7 +35,7 @@ public class BookController {
     private CategoryRepository crepository;
 
 
-    @RequestMapping(value="/students", method = RequestMethod.GET)
+    @RequestMapping(value="/api", method = RequestMethod.GET)
     public @ResponseBody List<Book> studentListRest() {	
         return (List<Book>) repository.findAll();
     }    
@@ -39,7 +45,7 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
-    @RequestMapping(value="/booklist/{id}", method = RequestMethod.GET)
+    @RequestMapping(value="/api/{id}", method = RequestMethod.GET)
     public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long Id) {    
         return repository.findById(Id);
     }      
@@ -53,12 +59,22 @@ public class BookController {
     public String save(Book book){
         repository.save(book);
         return "redirect:booklist";
+        
     }
+    @PreAuthorize("hasRole('a')")
+    @RequestMapping("/restricted")  
+    @ResponseBody  
+    public String restricted() {  
+        return "You found the secret lair!";  
+    }
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
-        repository.deleteById(bookId);
-        return "redirect:../booklist";
-    }
+    	repository.deleteById(bookId);
+    	return"redirect:../booklist";
+    	} 
+    
+
     @RequestMapping(value="/modify/{id}")
     public String editBook(@PathVariable("id") Long bookId, Model model) {
         model.addAttribute("book", repository.findById(bookId));
